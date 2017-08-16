c815a2ec4ef75ac1487cda82205f409713fa1b81
Igor <igor.bobro.lul@gmail.com>
2017-08-16 23:14:55 +0300

    Calc

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number:");
+            int num_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter operation:");
+            string oper = Console.ReadLine();
+
+            Console.WriteLine("Enter your second number:");
+            int num_2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
