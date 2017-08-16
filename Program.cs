ca2307d61cfa2916b638d583367a499c5fc75865
Oleksandr <global_Ol@mail.com>
2017-08-16 23:27:41 +0300

    added two methods - operation plus and minus

--- Program.cs
+++ Program.cs
@@ -18,6 +18,26 @@ namespace Calc
 
             Console.WriteLine("Enter your second number:");
             int num_2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = 0;
+            switch (oper)
+            {
+                case "+": result = Plus(num_1, num_2); break;
+                case "-": result = Minus(num_1, num_2); break;
+            }
+
+            Console.WriteLine(result);
+
+        }
+
+        static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Minus(int a, int b)
+        {
+            return a - b;
         }
     }
 }
